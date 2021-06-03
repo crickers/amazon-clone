fbe89a1f933f66fc5afd3f38aaf4f18925d5bc56
Cricky <crickmonster@gmail.com>
2021-06-03 19:21:42 -0400

    Initial Commit

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDUVxieyEAWkF9XQwHp-qOmodXSSiXMY5s",
+  authDomain: "challenge-5ab13.firebaseapp.com",
+  projectId: "challenge-5ab13",
+  storageBucket: "challenge-5ab13.appspot.com",
+  messagingSenderId: "662380504418",
+  appId: "1:662380504418:web:914c9dbef88f86f144f83b",
+  measurementId: "G-TFX0KC3CBK",
+};

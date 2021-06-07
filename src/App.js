441355009a1585f1713c72458b544ad093c6d45f
Cricky <crickmonster@gmail.com>
2021-06-07 19:05:40 -0400

    z-index causing error on header.css

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
+import React from "react";
 import "./App.css";
+import Header from './Header';
 
 function App() {
   return (
     //BEM naming convention
     <div className="app">
       <h1>Crickbuild: Amazon Store!</h1>
-      {/*Header*/}
+      <Header/>
+     
       {/*Home*/}
     </div>
   );

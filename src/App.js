2bc08995534b8c9d52e79abfe04bdcdf45a3b9b1
Cricky <crickmonster@gmail.com>
2021-06-07 20:22:04 -0400

    material-ui search icon attempt

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     //BEM naming convention
     <div className="app">
-      <h1>Crickbuild: Amazon Store!</h1>
+    <h1>Crickbuilt: Amazon.com</h1>
       <Header/>
      
       {/*Home*/}
